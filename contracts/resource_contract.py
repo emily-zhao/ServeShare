8d4d73b9cb1a06f47e2d26d6e770709d90a91de8
Maximilian Evans <156max@gmail.com)>
2020-11-14 15:41:31 -0500

    added our own custom contract!

---
+++ contracts/resource_contract.py
@@ -0,0 +1,25 @@
+
+balances = Hash(default_value=0)
+
+# offer_id -> { ip, price }
+resource_offers = Hash(default_value=None)
+
+@export
+def create_offer(price: int, address: str):
+    sender = ctx.caller
+    resource_offers[sender] = {'price': price, 'address': address}
+
+@export
+def purchase_offer(offer: str):
+    sender = ctx.caller
+    assert balances[sender] >= resource_offers[offer]['price'],\
+    "Offer price exceeds available balance."
+    balances[sender] -= resource_offers[offer]['price']
+    balances[offer] += resource_offers[offer]['price']
+    resource_offers['purchaser'] = sender
+
+@export
+def free_cash(amount: int):
+    sender = ctx.caller
+    balances[sender] += amount
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,28 @@
 import ContractClient from './ContractClient';
 
-let client = new ContractClient();
-client.getAllContracts().then(contracts => {
-    console.log(contracts);
-});
-window.contractClient = client;
+let printfn = x => console.log(x);
+
+let meClient = new ContractClient('me');
+let otherClient = new ContractClient('other');
+meClient.getAllContracts().then(printfn);
+window.contractClient = meClient;
+
+Promise.resolve().then(
+    meClient.runTransaction('resource_contract', 'free_cash', {'amount':10}).then(printfn)
+).then(
+    meClient.getContractVariable('resource_contract', 'balances', 'me').then(printfn)
+).then(
+    otherClient.runTransaction('resource_contract','create_offer', {'price':5, 'address': '1234'}).then(printfn)
+).then(
+    otherClient.getContractVariable('resource_contract', 'balances', 'other').then(printfn)
+).then(
+    otherClient.getContractVariable('resource_contract', 'resource_offers', 'other').then(printfn)
+).then(
+    meClient.runTransaction('resource_contract', 'purchase_offer', {'offer': 'other'}).then(printfn)
+).then(
+    meClient.getContractVariable('resource_contract', 'balances', 'me').then(printfn)
+).then(
+    otherClient.getContractVariable('resource_contract', 'balances', 'other').then(printfn)
+).then(
+    otherClient.getContractVariable('resource_contract', 'resource_offers', 'other').then(printfn)
+);
