9720bb19029e1505ba1dd35fa70e1a200eb4e051
Maximilian Evans <156max@gmail.com)>
2020-11-14 23:51:16 -0500

    finished the scripts for the hosting page

--- frontend/src/host.js
+++ frontend/src/host.js
@@ -1,7 +1,25 @@
 import WalletContractClient from './WalletContractClient';
 
+function showActiveOffer(client){
+    client.getMyOffer().then(offer => {
+        console.log(offer);
+        document.getElementById('activeOffer').innerHTML = offer;
+    });
+}
+
 window.onload = () => {
     let contractClient = new WalletContractClient();
     //for debugging
     window.contractClient = contractClient;
+    
+    showActiveOffer(contractClient);
+    
+    document.getElementById('hostButton').onclick = () => {
+        let address = document.getElementById('InputAddress').value;
+        let price = parseInt(document.getElementById('InputPrice').value);
+        document.getElementById('activeOffer').innerHTML = 'sending transaction...';
+        contractClient.createOffer(price, address).then(() => {
+            showActiveOffer(contractClient);
+        });;
+    };
 };
